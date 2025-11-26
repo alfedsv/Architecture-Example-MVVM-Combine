cc9b49835d4f9bf2a5a5cac8d429f0408ab47269
alexfedsv <alfedsv@yandex.ru>
2025-11-26 08:50:14 +0300

    init commit

---
+++ Architecture-Example-MVVM-Combine/Architecture-Example-MVVM-Combine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Architecture-Example-MVVM-Combine
+//
+//  Created by  Alexander Fedoseev on 26.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
